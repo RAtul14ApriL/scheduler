b760ee0aa091d56071e7b77f60765eb6e3a7bff7
RAtul14ApriL <ratul14april@gmail.com>
2022-01-25 18:05:37 -0600

    Added transition for showing Delete and Save error message

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Show(props) {
 
   const { student, interviewer, onEdit, onDelete } = props;
-
+console.log("SHOW", props);
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
